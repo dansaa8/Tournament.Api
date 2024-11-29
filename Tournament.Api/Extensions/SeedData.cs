35a4b16fad9fb6321db71aef9fa84054327d546b
dansaa8 <dansaa8@protonmail.com>
2024-11-29 22:57:16 +0100

    Update seedData

--- Tournament.Api/Extensions/SeedData.cs
+++ Tournament.Api/Extensions/SeedData.cs
@@ -2,7 +2,6 @@
 using Tournament.Data.Data;
 using Tournament.Core.Entities;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Tournament.Api.Extensions
@@ -18,15 +17,20 @@ namespace Tournament.Api.Extensions
 
                 await db.Database.MigrateAsync();
 
-                // Kolla om data redan finns, isåfall behövs ingen seedning.
+                // Check if data already exists, if yes, no need to seed.
                 if (await db.TournamentDetails.AnyAsync()) return;
 
                 try
                 {
-                    var tournaments = GenerateTournamentsWithGames();
-                    db.AddRange(tournaments);
+                    // Create tournaments
+                    var tournaments = GenerateTournaments();
+                    db.TournamentDetails.AddRange(tournaments);
+                    await db.SaveChangesAsync(); // Save tournaments to get their Ids
 
-                    await db.SaveChangesAsync();
+                    // Create games
+                    var games = GenerateGames(tournaments);
+                    db.Games.AddRange(games);
+                    await db.SaveChangesAsync(); // Save games
                 }
                 catch (Exception ex)
                 {
@@ -35,51 +39,79 @@ namespace Tournament.Api.Extensions
             }
         }
 
-        private static IEnumerable<TournamentDetails> GenerateTournamentsWithGames()
+        private static List<TournamentDetails> GenerateTournaments()
         {
             return new List<TournamentDetails>
             {
                 new TournamentDetails
                 {
                     Title = "Winter Championship 2024",
-                    StartDate = new DateTime(2024, 1, 15),
-                    Games = new List<Game>
-                    {
-                        new Game { Title = "Opening Match", Time = new DateTime(2024, 1, 15, 10, 0, 0) },
-                        new Game { Title = "Semi-Final", Time = new DateTime(2024, 1, 18, 14, 30, 0) }
-                    }
+                    StartDate = new DateTime(2024, 1, 15)
                 },
                 new TournamentDetails
                 {
                     Title = "Spring Invitational 2024",
-                    StartDate = new DateTime(2024, 4, 10),
-                    Games = new List<Game>
-                    {
-                        new Game { Title = "Group Stage", Time = new DateTime(2024, 4, 10, 9, 0, 0) },
-                        new Game { Title = "Final Match", Time = new DateTime(2024, 4, 15, 17, 0, 0) }
-                    }
+                    StartDate = new DateTime(2024, 4, 10)
                 },
                 new TournamentDetails
                 {
                     Title = "Summer Clash 2024",
-                    StartDate = new DateTime(2024, 7, 5),
-                    Games = new List<Game>
-                    {
-                        new Game { Title = "Opening Round", Time = new DateTime(2024, 7, 5, 12, 0, 0) },
-                        new Game { Title = "Championship Match", Time = new DateTime(2024, 7, 10, 16, 0, 0) }
-                    }
-                },
-                new TournamentDetails
-                {
-                    Title = "Autumn League 2024",
-                    StartDate = new DateTime(2024, 10, 20),
-                    Games = new List<Game>
-                    {
-                        new Game { Title = "Round Robin", Time = new DateTime(2024, 10, 20, 8, 0, 0) },
-                        new Game { Title = "Playoffs", Time = new DateTime(2024, 10, 25, 15, 30, 0) }
-                    }
+                    StartDate = new DateTime(2024, 7, 5)
                 }
             };
+        }
+
+        private static List<Game> GenerateGames(List<TournamentDetails> tournaments)
+        {
+            var games = new List<Game>();
+
+            var tournament1 = tournaments[0];
+            var tournament2 = tournaments[1];
+            var tournament3 = tournaments[2];
+
+            games.Add(new Game
+            {
+                Title = "Winter Championship Opening Match",
+                Time = new DateTime(2024, 1, 15, 10, 0, 0),
+                TournamentId = tournament1.Id
+            });
+
+            games.Add(new Game
+            {
+                Title = "Winter Championship Semi-Final",
+                Time = new DateTime(2024, 1, 18, 14, 30, 0),
+                TournamentId = tournament1.Id
+            });
+
+            games.Add(new Game
+            {
+                Title = "Spring Invitational Group Stage",
+                Time = new DateTime(2024, 4, 10, 9, 0, 0),
+                TournamentId = tournament2.Id
+            });
+
+            games.Add(new Game
+            {
+                Title = "Spring Invitational Final Match",
+                Time = new DateTime(2024, 4, 15, 17, 0, 0),
+                TournamentId = tournament2.Id
+            });
+
+            games.Add(new Game
+            {
+                Title = "Summer Clash Opening Round",
+                Time = new DateTime(2024, 7, 5, 12, 0, 0),
+                TournamentId = tournament3.Id
+            });
+
+            games.Add(new Game
+            {
+                Title = "Summer Clash Championship Match",
+                Time = new DateTime(2024, 7, 10, 16, 0, 0),
+                TournamentId = tournament3.Id
+            });
+
+            return games;
         }
     }
 }
