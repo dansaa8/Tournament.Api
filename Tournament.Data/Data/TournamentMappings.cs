4dd5693ede85fce4b0cbd453c8fcd8df7fc12fa2
dansaa8 <dansaa8@protonmail.com>
2024-11-30 21:12:28 +0100

    Add a mapper that will hold all mapping declarations from entity to dto and viceversa etc.

---
+++ Tournament.Data/Data/TournamentMappings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Tournament.Core.Dto;
+using Tournament.Core.Entities;
+
+namespace Tournament.Data.Data
+{
+    public class TournamentMappings : Profile
+    {
+        public TournamentMappings()
+        {
+            CreateMap<Game, GameDto>();
+            CreateMap<TournamentDetails, TournamentDto>();
+        }
+    }
+}
