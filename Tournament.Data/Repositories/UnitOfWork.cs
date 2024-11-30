e3225be48c6f1941de7a17770f9053ddbeac0ac6
dansaa8 <dansaa8@protonmail.com>
2024-11-30 14:59:34 +0100

    Add IUoW and implement it in a UoW class that uses repositories.

---
+++ Tournament.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Repositories;
+using Tournament.Data.Data;
+
+namespace Tournament.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TournamentApiContext _context;
+        public ITournamentRepository TournamentRepository { get; }
+        public IGameRepository GameRepository { get; }
+
+        public UnitOfWork(TournamentApiContext context)
+        {
+            _context = context;
+            TournamentRepository = new TournamentRepository(context);
+            GameRepository = new GameRepository(context);
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
