d669f306ce078ecc6ab4eb3df6c96e18af754d79
dansaa8 <dansaa8@protonmail.com>
2024-11-30 21:11:50 +0100

    Add one output-dto for each entity (TournamentDetails & Game) that will be used in each controller to convert a real entity into a dto before leaving the controller and sent as a response to the requester.

---
+++ Tournament.Core/Dto/GameDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    public record GameDto
+    {
+        public string? Title { get; init; }
+        public string? Time { get; init; }
+    }
+}
---
+++ Tournament.Core/Dto/TournamentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    public record TournamentDto
+    {
+        public string? Title { get; init; }
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate => StartDate.AddMonths(3);
+    }
+}
